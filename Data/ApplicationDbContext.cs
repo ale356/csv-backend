9a4eb2a51244408abc62f302c1f9bb0f1f2ed731
Alejandro <al225vh@student.lnu.se>
2024-10-30 11:24:53 +0100

    Add the context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using CsvUploadApp.Models;
+
+namespace csv-backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
