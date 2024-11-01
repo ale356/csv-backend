54fe9dc78594bbb45a7ae07b24f16840783b1dbf
Alejandro <al225vh@student.lnu.se>
2024-11-01 11:49:39 +0100

    Prepare project for back-end

--- obj/Debug/net8.0/csv-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/csv-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csv-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b6d71de60f6d64150371f2e1e7b6d7ef6598d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b7e4aa7cd333ed55de41db13ae19f0283885ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("csv-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csv-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
